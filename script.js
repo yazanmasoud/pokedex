09886ddee34102e8b0da9b82d6f208145f4bbb7c
Yazan Masoud <yazanmas895@gmail.com>
2026-03-09 07:02:26 +0100

    render first 100 pokemon from API

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 async function loadPokemon() {
-    let response = await fetch("https://pokeapi.co/api/v2/pokemon/pikachu");
+    let response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=100&offset=0");
     let responseToJson = await response.json();
-    console.log(responseToJson);
-    document.getElementById('pokemon-container').innerHTML += getPokemonTemplate(responseToJson);   
+    let pokemonList = responseToJson.results;
+
+    for (let i = 0; i < pokemonList.length; i++) {
+        const pokemon = pokemonList[i];
+        let pokemonResponse = await fetch(pokemon.url);
+        let pokemonResponseToJson = await pokemonResponse.json();
+        console.log(pokemonResponseToJson);
+        document.getElementById('pokemon-container').innerHTML += getPokemonTemplate(pokemonResponseToJson);
+        
+        
+    }
 }
