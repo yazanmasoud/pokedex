10b85a8e01ce4741db0a7ee78743d351fe5f69a1
Yazan Masoud <yazanmas895@gmail.com>
2026-03-11 20:50:24 +0100

    Enhance Pokemon dialog functionality and styling, including improved event handling and layout adjustments

--- script.js
+++ script.js
@@ -8,12 +8,15 @@ function init() {
 async function loadPokemon() {
     let response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=100&offset=0");
     let responseToJson = await response.json();
+    
     let pokemonList = responseToJson.results;
 
     for (let i = 0; i < pokemonList.length; i++) {
         const pokemon = pokemonList[i];
-        let pokemonResponse = await fetch(pokemon.url);
+        let pokemonResponse = await fetch(pokemon.url);        
         let pokemonResponseToJson = await pokemonResponse.json();
+        console.log(pokemonResponseToJson);
+        
         allPokemon.push(pokemonResponseToJson);
 
         document.getElementById('pokemon-container').innerHTML += getPokemonTemplate(pokemonResponseToJson, i);
@@ -21,10 +24,15 @@ async function loadPokemon() {
 }
 
 
-function togglePokemonDialog(index) {
+function openPokemonDialog(index) {
     let pokemon = allPokemon[index];
     const pokemonDialog = document.getElementById('pokemon-dialog');
     pokemonDialog.innerHTML = getPokemonDialogTemplate(pokemon , index);
     console.log(allPokemon);
-    pokemonDialog.open ? pokemonDialog.close() : pokemonDialog.showModal();
+    pokemonDialog.showModal();
+}
+
+function closePokemonDialog() {
+    const pokemonDialog = document.getElementById('pokemon-dialog');
+    pokemonDialog.close();
 }
--- scripts/templates.js
+++ scripts/templates.js
@@ -1,7 +1,7 @@
 function getPokemonTemplate(pokemonObject, index) {
     let type = pokemonObject.types[0].type.name;
     return `
-    <div onclick ="togglePokemonDialog(${index})" class="pokemonCard">
+    <div onclick ="openPokemonDialog(${index})" class="pokemonCard">
         <h2 class="pokemonName"><b>#${pokemonObject.id}</b> ${pokemonObject.name}</h2> 
         <img class="pokemonImage" src="${pokemonObject.sprites.front_default}">
         <div class="cardFooter">
@@ -11,12 +11,20 @@ function getPokemonTemplate(pokemonObject, index) {
     `;
 }
 
-function getPokemonDialogTemplate(pokemon , index) {
+function getPokemonDialogTemplate(pokemon, index) {
     return `
+    <div class="pokemonDialogInner">
     <div class="dialogHeader">
-        <button onclick ="togglePokemonDialog(${index})"><img class="esc-img" src="./icons/delete.svg" alt=""></button>
+        <h2>${pokemon.name}</h2>
+        <button onclick ="closePokemonDialog(${index})"><img class="esc-img" src="./icons/delete.svg" alt="Pokemon Dialog photo"></button>
     </div>
-    <h2>${pokemon.name}</h2>
-    <img src="${pokemon.sprites.front_default}" alt="" />
+    
+    <img class ="dialogImage" src="${pokemon.sprites.other["official-artwork"].front_default}" alt="" />
+    <div class="pokemonInDialog">
+        <span><strong>height: </strong>${pokemon.height}</span>
+        <span><strong>weight: </strong>${pokemon.weight}</span>
+        <span><strong>id: </strong>${pokemon.id}</span>
+    </div>
+    </div>
     `;
 }
