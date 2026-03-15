60afcec866b74cee19f9f7ae73bf19efc140e79a
Yazan Masoud <yazanmas895@gmail.com>
2026-03-15 18:28:06 +0100

    Add Pokemon dialog enhancements with navigation and updated styles

--- script.js
+++ script.js
@@ -8,17 +8,16 @@ function init() {
 async function loadPokemon() {
     let response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=100&offset=0");
     let responseToJson = await response.json();
-    
+
     let pokemonList = responseToJson.results;
 
     for (let i = 0; i < pokemonList.length; i++) {
         const pokemon = pokemonList[i];
-        let pokemonResponse = await fetch(pokemon.url);        
+        let pokemonResponse = await fetch(pokemon.url);
         let pokemonResponseToJson = await pokemonResponse.json();
-        console.log(pokemonResponseToJson);
         
         allPokemon.push(pokemonResponseToJson);
-
+        
         document.getElementById('pokemon-container').innerHTML += getPokemonTemplate(pokemonResponseToJson, i);
     }
 }
@@ -26,13 +25,30 @@ async function loadPokemon() {
 
 function openPokemonDialog(index) {
     let pokemon = allPokemon[index];
+    console.log(pokemon);
+    
     const pokemonDialog = document.getElementById('pokemon-dialog');
-    pokemonDialog.innerHTML = getPokemonDialogTemplate(pokemon , index);
-    console.log(allPokemon);
+    pokemonDialog.innerHTML = getPokemonDialogTemplate(pokemon, index);
     pokemonDialog.showModal();
 }
 
 function closePokemonDialog() {
+    console.log("dialog closed");
     const pokemonDialog = document.getElementById('pokemon-dialog');
     pokemonDialog.close();
+}
+
+function openDialogStatus() {
+    let pokemonmain = document.getElementById('pokemon-main');
+    let pokemonStatus = document.getElementById('pokemon-status');
+    pokemonmain.classList.add("none");
+    pokemonStatus.classList.remove("none");
+}
+
+function openDialogMain() {
+    let pokemonmain = document.getElementById('pokemon-main');
+    let pokemonStatus = document.getElementById('pokemon-status');
+    pokemonmain.classList.remove("none");
+    pokemonStatus.classList.add("none");
+
 }
--- scripts/templates.js
+++ scripts/templates.js
@@ -1,11 +1,13 @@
-function getPokemonTemplate(pokemonObject, index) {
-    let type = pokemonObject.types[0].type.name;
+function getPokemonTemplate(pokemon, index) {
+    const type = pokemon.types[0].type.name;
+
     return `
-    <div onclick ="openPokemonDialog(${index})" class="pokemonCard">
-        <h2 class="pokemonName"><b>#${pokemonObject.id}</b> ${pokemonObject.name}</h2> 
-        <img class="pokemonImage" src="${pokemonObject.sprites.front_default}">
+    <div onclick="openPokemonDialog(${index})" class="pokemonCard">
+        <h2 class="pokemonName"><b>#${pokemon.id}</b> ${pokemon.name}</h2> 
+        <img class="pokemonImage" src="${pokemon.sprites.front_default}">
+        
         <div class="cardFooter">
-        <img id= "type-icon" class="typeIcon" src="./icons/${type}.svg">
+            <img class="typeIcon" src="./icons/${type}.svg">
         </div>
     </div>
     `;
@@ -13,18 +15,65 @@ function getPokemonTemplate(pokemonObject, index) {
 
 function getPokemonDialogTemplate(pokemon, index) {
     return `
-    <div class="pokemonDialogInner">
-    <div class="dialogHeader">
-        <h2 class="dialogPokemonName">${pokemon.name}</h2>
-        <button onclick ="closePokemonDialog(${index})"><img class="esc-img" src="./icons/delete.svg" alt="Pokemon Dialog photo"></button>
-    </div>
-    
-    <img class ="dialogImage" src="${pokemon.sprites.other["official-artwork"].front_default}" alt="" />
-    <div class="pokemonInDialog">
-        <span><strong>height: </strong>${(pokemon.height * 0.1).toFixed(1)} M</span>
-        <span><strong>weight: </strong>${(pokemon.weight) * 0.1.toFixed(2)} KG</span>
-        <span><strong>Base-experience: </strong>${pokemon.base_experience}</span>
-    </div>
+    <div class="pokemonDialogInner" onclick="event.stopPropagation()">
+        <div class="dialogHeader">
+            <h2 class="dialogPokemonName">${pokemon.name}</h2>
+            <button onclick="closePokemonDialog()">
+                <img class="esc-img" src="./icons/delete.svg" alt="">
+            </button>
+        </div>
+
+        <img class="dialogImage" src="${pokemon.sprites.other["official-artwork"].front_default}" />
+
+        <nav class="dialogNavBar">
+            <a onclick="openDialogMain()">Main</a>
+            <a onclick="openDialogStatus()">Status</a>
+        </nav>
+
+        <div id="pokemon-main" class="pokemonMain">
+            <span><strong>Height:</strong> ${(pokemon.height * 0.1).toFixed(1)} m</span>
+            <span><strong>Weight:</strong> ${(pokemon.weight * 0.1).toFixed(2)} kg</span>
+            <span><strong>Base experience:</strong> ${pokemon.base_experience}</span>
+        </div>
+
+        <div id="pokemon-status" class="pokemonStatus none">
+            <div class= "progressBar">
+              <div class="progressBarInner" style="height:24px;width:${(allPokemon[index].stats[0].base_stat) / 2}%" >
+                <strong>hp: </strong> ${allPokemon[index].stats[0].base_stat}
+              </div>
+            </div>
+            <div class= "progressBar">
+              <div class="progressBarInner" style="background-color: #af3b3bff; height:24px;width:${(allPokemon[index].stats[1].base_stat) / 2}%" >
+                <strong>attack: </strong> ${allPokemon[index].stats[1].base_stat}
+              </div>
+            </div>
+            <div class= "progressBar">
+              <div class="progressBarInner" style=" background-color: #a49e25ff; height:24px;width:${(allPokemon[index].stats[2].base_stat) / 2}%" >
+                <strong>defence: </strong> ${allPokemon[index].stats[2].base_stat}
+              </div>
+            </div>
+            <div class= "progressBar">
+              <div class="progressBarInner" style="background-color: #870909ff; height:24px;width:${(allPokemon[index].stats[3].base_stat) / 2}%" >
+                <strong>special-attack: </strong> ${allPokemon[index].stats[3].base_stat}
+              </div>
+            </div>
+            <div class= "progressBar">
+              <div class="progressBarInner" style="background-color: #696e0aff; height:24px;width:${(allPokemon[index].stats[4].base_stat) / 2}%" >
+                <strong>special-defence: </strong> ${allPokemon[index].stats[4].base_stat}
+              </div>
+            </div>
+            <div class= "progressBar">
+              <div class="progressBarInner" style="background-color: #008df1ff; height:24px;width:${(allPokemon[index].stats[5].base_stat) / 2}%" >
+                <strong>speed: </strong> ${allPokemon[index].stats[5].base_stat}
+              </div>
+            </div>
+        </div>
+        <div class ="prevNextButtons">
+                <button><img src="./icons/arrow.png" alt="left arrow" /></button>
+                <button><img src="./icons/right-arrow.png" alt="right arrow" /></button>   
+        </div>
     </div>
     `;
 }
+
+
